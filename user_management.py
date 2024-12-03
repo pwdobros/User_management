6d8bd8b612b6c4cd1d86e89cc71715b71a3208f9
Konrad Dobros <konraddob@gmail.com>
2024-12-03 09:26:18 +0100

    First steps

--- user_management.py
+++ user_management.py
@@ -3,3 +3,14 @@ import random
 import re
 import os
 
+def add_user(user_data):
+    
+
+def save_users_to_file(users):
+
+def validate_nip(nip):
+
+def validate_pesel(pesel):
+
+def validate_regon(regon):
+
